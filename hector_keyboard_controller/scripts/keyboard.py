d31c622b17d88e7d96eea6527851d93e567f11a2
AbdealiJK <abdealikothari@gmail.com>
2016-01-29 10:48:16 +0530

    Add tf and show RPY in hector keyboard

--- hector_keyboard_controller/scripts/keyboard.py
+++ hector_keyboard_controller/scripts/keyboard.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy, tf
 import time, sys, math
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Twist, Pose, Quaternion
 from gazebo_msgs.msg import ModelStates
 import pygame
 
@@ -15,7 +15,7 @@ class KeyboardController:
 
     def _gui_init(self):
         pygame.init()
-        self.screen = pygame.display.set_mode([400, 600])
+        self.screen = pygame.display.set_mode([400, 500])
         self.clock = pygame.time.Clock()
 
         background = pygame.Surface(self.screen.get_size())
@@ -79,61 +79,88 @@ class KeyboardController:
 
     def _event(self, key_events):
         dx, dy, dz = 0.1, 0.1, 0.1
+        daz = 0.01
 
         if key_events[pygame.K_UP]:
-            self.msg.linear.z += dz;
+            self.msg.linear.z += dz
         elif key_events[pygame.K_DOWN]:
-            self.msg.linear.z -= dz;
+            self.msg.linear.z -= dz
 
         if key_events[pygame.K_w]:
-            self.msg.linear.y += dy;
+            self.msg.linear.y += dy
         elif key_events[pygame.K_s]:
-            self.msg.linear.y -= dy;
+            self.msg.linear.y -= dy
 
         if key_events[pygame.K_d]:
-            self.msg.linear.x += dx;
+            self.msg.linear.x += dx
         elif key_events[pygame.K_a]:
-            self.msg.linear.x -= dx;
+            self.msg.linear.x -= dx
+
+        if key_events[pygame.K_i]:
+            self.msg.angular.z += daz
+        elif key_events[pygame.K_k]:
+            self.msg.angular.z -= daz
 
     def _draw(self):
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
         self.background.fill((250, 250, 250))
 
-        self._draw_y = 20
-        self._draw_x = self.background.get_rect().centerx
+        width = float(self.background.get_rect().width)
 
+        self._draw_y = 20
+        self._draw_x = width / 2
         self._draw_text("Keyboard controller", 40)
+
+        old_draw_y = self._draw_y
+        self._draw_x = width / 4
         self._draw_text("Linear vel", 30)
         self._draw_text("X (d/a) : %.3f" % self.msg.linear.x)
         self._draw_text("Y (w/s) : %.3f" % self.msg.linear.y)
         self._draw_text("Z (up/down) : %.3f" % self.msg.linear.z)
+
+        self._draw_y = old_draw_y # On the side
+        self._draw_x = width * 3 / 4
         self._draw_text("Angular vel", 30)
         self._draw_text("X : %.3f" % self.msg.angular.x)
         self._draw_text("Y : %.3f" % self.msg.angular.y)
-        self._draw_text("Z : %.3f" % self.msg.angular.z)
+        self._draw_text("Z (i/k) : %.3f" % self.msg.angular.z)
 
+        self._draw_x = width / 2
         self._draw_text("Quadrotor state", 40)
 
         old_draw_y = self._draw_y
-        self._draw_x = self.background.get_rect().centerx / 2
-
+        self._draw_x = width / 4
         self._draw_text("Position", 30)
         self._draw_text("X : %.3f" % self.quad_pose.position.x)
         self._draw_text("Y : %.3f" % self.quad_pose.position.y)
         self._draw_text("Z : %.3f" % self.quad_pose.position.z)
+
+        self._draw_text("Linear velocity", 30)
+        self._draw_text("X : %.3f" % self.quad_twist.linear.x)
+        self._draw_text("Y : %.3f" % self.quad_twist.linear.y)
+        self._draw_text("Z : %.3f" % self.quad_twist.linear.z)
+
+        self._draw_y = old_draw_y # On the side
+        self._draw_x = width * 3 / 4
         self._draw_text("Orientation", 30)
+
+        old_draw_y = self._draw_y
+        self._draw_x = width * 5 / 8
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion(
+            self.msg_to_quaternion(self.quad_pose.orientation))
+        self._draw_text("R : %.3f" % math.degrees(roll))
+        self._draw_text("P : %.3f" % math.degrees(pitch))
+        self._draw_text("Y : %.3f" % math.degrees(yaw))
+
+        self._draw_y = old_draw_y
+        self._draw_x = width * 7 / 8
         self._draw_text("X : %.3f" % self.quad_pose.orientation.x)
         self._draw_text("Y : %.3f" % self.quad_pose.orientation.y)
         self._draw_text("Z : %.3f" % self.quad_pose.orientation.z)
         self._draw_text("W : %.3f" % self.quad_pose.orientation.w)
 
-        self._draw_y = old_draw_y
-        self._draw_x = self._draw_x + self.background.get_rect().centerx
-        self._draw_text("Linear velocity", 30)
-        self._draw_text("X : %.3f" % self.quad_twist.linear.x)
-        self._draw_text("Y : %.3f" % self.quad_twist.linear.y)
-        self._draw_text("Z : %.3f" % self.quad_twist.linear.z)
+        self._draw_x = width * 3 / 4
         self._draw_text("Angular velocity", 30)
         self._draw_text("X : %.3f" % self.quad_twist.angular.x)
         self._draw_text("Y : %.3f" % self.quad_twist.angular.y)
@@ -155,6 +182,11 @@ class KeyboardController:
 
         self._draw_y += font_size + 5
 
+    @staticmethod
+    def msg_to_quaternion(q):
+        assert type(q) == Quaternion
+        # Quaternions in TF are normally SXYZ form
+        return [q.w, q.x, q.y, q.z]
 
     def _sleep(self):
         self.rate.sleep()
